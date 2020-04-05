4778b494035175edd1563d07224fa8fb5f2fcba0
Marcell <banuma@freemail.hu>
2020-04-05 14:06:50 +0100

    adjust the code

--- js/app.js
+++ js/app.js
@@ -8,54 +8,43 @@
     let finalPairing = [];
     let winner = d.getElementById("winner");
 
+    button.addEventListener ("click", () => {
 
-     button.addEventListener ("click", () => {
+    store.push(input.value);
 
-         store.push(input.value);
-        
-        let item = d.createElement("li");
-        item.append("Player " + count + ": " + input.value);
-        d.getElementById("list-player").append(item);
-        input.value = "";
-        count++; 
+    let item = d.createElement("li");
+    item.append("Player " + count + ": " + input.value);
+    d.getElementById("list-player").append(item);
+    input.value = "";
+    count++; 
 
-     });
+    });
 
-     generate.addEventListener("click", (evt) => {
-
+    generate.addEventListener("click", (evt) => {
         let n = 2;
         let randomItem = [];
         let temp = store;
-        
-        for (let i = 0; i < store.length; i += 2) { 
- 
-        randomItem = temp.sort(() => Math.random() - Math.random()).slice(0, n);
 
-        finalPairing.push(randomItem);
-
-        temp = temp.filter(v => !randomItem.includes(v));      
-     
+        for (let i = 0; i < store.length; i += 2) {  
+            randomItem = temp.sort(() => Math.random() - Math.random()).slice(0, n);
+            finalPairing.push(randomItem);
+            temp = temp.filter(v => !randomItem.includes(v));
         }
-   
+
         for (let j = 0; j < finalPairing.length; j++) {
             let item = d.createElement("li");
             item.append("Pairing: " + finalPairing[j][0] + " vs " + finalPairing[j][1]);
             d.getElementById("list-pairs").append(item);
         }
-        
-    winner.addEventListener("click", () => {
+    });
 
-    
+    winner.addEventListener("click", () => {
         for (let i = 0; i < finalPairing.length; i++) {
             winner = finalPairing[i][Math.floor(Math.random()*finalPairing[i].length)];
             let item = d.createElement("li");
             item.append(winner);
             d.getElementById("list-win").append(item)
         }
-            
-        });
-
     });
-
 
 })(document);

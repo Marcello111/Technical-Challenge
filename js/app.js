02dc5a12a1098ab8c94fcd8452741f07840a14cb
Marcell <banuma@freemail.hu>
2020-04-16 11:26:01 +0100

    prevent unexpected user interaction

--- js/app.js
+++ js/app.js
@@ -8,43 +8,74 @@
     let finalPairing = [];
     let winner = d.getElementById("winner");
 
+    // Add player's name
     button.addEventListener ("click", () => {
 
-    store.push(input.value);
+        // Prevent unexpected user interaction if there is no name submitted
+        if (input.value.length == 0) {
+            alert("Please enter the player's name");
+        } else {
 
-    let item = d.createElement("li");
-    item.append("Player " + count + ": " + input.value);
-    d.getElementById("list-player").append(item);
-    input.value = "";
-    count++; 
+            store.push(input.value);
+
+            let item = d.createElement("li");
+            item.append("Player " + count + ": " + input.value);
+            d.getElementById("list-player").append(item);
+            input.value = "";
+            count++; 
+            }
 
     });
 
+    // Generate pairs randomly
     generate.addEventListener("click", (evt) => {
         let n = 2;
         let randomItem = [];
         let temp = store;
 
-        for (let i = 0; i < store.length; i += 2) {  
-            randomItem = temp.sort(() => Math.random() - Math.random()).slice(0, n);
-            finalPairing.push(randomItem);
-            temp = temp.filter(v => !randomItem.includes(v));
+        // Prevent unexpected user interaction if there is odd number of players submitted    
+        if (store.length % 2 == 1) {
+            alert("The submitted players' number must be even");
+            
+        } else {
+
+            for (let i = 0; i < store.length; i += 2) { 
+
+                // Choose 2 random players
+                randomItem = temp.sort(() => Math.random() - Math.random()).slice(0, n);
+
+                // Add 2 paired players to the finalPairing array
+                finalPairing.push(randomItem);
+
+                // Store the rest of players except the 2 previously paired players
+                temp = temp.filter(v => !randomItem.includes(v));
+            }
+
         }
 
+        // Put paired players into the list
         for (let j = 0; j < finalPairing.length; j++) {
+
             let item = d.createElement("li");
             item.append("Pairing: " + finalPairing[j][0] + " vs " + finalPairing[j][1]);
             d.getElementById("list-pairs").append(item);
         }
+
     });
 
+    // Generate the winners randomly
     winner.addEventListener("click", () => {
+
         for (let i = 0; i < finalPairing.length; i++) {
+
+            // Choose 1 random player from the finalPairing array
             winner = finalPairing[i][Math.floor(Math.random()*finalPairing[i].length)];
+
             let item = d.createElement("li");
             item.append(winner);
             d.getElementById("list-win").append(item)
         }
+
     });
 
 })(document);
